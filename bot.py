2c59f7c6191abef053764cb050b3127a5c11731c
Sriram Ananthakrishnan <srirama1@uchicago.edu>
2025-04-02 22:22:17 -0500

    template

--- bot.py
+++ bot.py
@@ -1,3 +1,28 @@
-#TO DO - CREATE BOT
+from typing import Optional
+
+import pandas as pd
+import asyncio
+import argparse
+
+
+# class MyXchangeClient(xchange_client.XChangeClient):
+#     '''A shell client with the methods that can be implemented to interact with the xchange.'''
+
+#     def __init__(self, host: str, username: str, password: str):
+#         super().__init__(host, username, password)
+
+#     async def start(self, user_interface):
+#         asyncio.create_task(self.trade())
+
+#         # This is where Phoenixhood will be launched if desired. There is no need to change these
+#         # lines, you can either remove the if or delete the whole thing depending on your purposes.
+#         if user_interface:
+#             self.launch_user_interface()
+#             asyncio.create_task(self.handle_queued_messages())
+
+#         await self.connect()
+
+
+print('bruh')
 
 
