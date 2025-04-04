fd4e31877b5633845a7c091103c76656452bc8f9
Sriram Ananthakrishnan <srirama1@uchicago.edu>
2025-04-04 09:44:23 -0500

    stub code

--- bot.py
+++ bot.py
@@ -1,26 +1,15 @@
 from typing import Optional
 
+import xchange_client
 import asyncio
 import argparse
-from utcxchangelib import xchange_client
+
 
 class MyXchangeClient(xchange_client.XChangeClient):
-    '''A shell client with the methods that can be implemented to interact with the xchange.'''
 
     def __init__(self, host: str, username: str, password: str):
         super().__init__(host, username, password)
 
-    async def start(self, user_interface):
-        asyncio.create_task(self.trade())
-
-        # This is where Phoenixhood will be launched if desired. There is no need to change these
-        # lines, you can either remove the if or delete the whole thing depending on your purposes.
-        if user_interface:
-            self.launch_user_interface()
-            asyncio.create_task(self.handle_queued_messages())
-
-        await self.connect()
-    
     async def bot_handle_cancel_response(self, order_id: str, success: bool, error: Optional[str]) -> None:
         order = self.open_orders[order_id]
         print(f"{'Market' if order[2] else 'Limit'} Order ID {order_id} cancelled, {order[1]} unfilled")
@@ -30,6 +19,7 @@ class MyXchangeClient(xchange_client.XChangeClient):
 
     async def bot_handle_order_rejected(self, order_id: str, reason: str) -> None:
         print("order rejected because of ", reason)
+
 
     async def bot_handle_trade_msg(self, symbol: str, price: int, qty: int):
         pass
@@ -41,6 +31,7 @@ class MyXchangeClient(xchange_client.XChangeClient):
         pass
 
     async def bot_handle_news(self, news_release: dict):
+
         # Parsing the message based on what type was received
         timestamp = news_release["timestamp"] # This is in exchange ticks not ISO or Epoch
         news_type = news_release['kind']
@@ -50,17 +41,85 @@ class MyXchangeClient(xchange_client.XChangeClient):
             subtype = news_data["structured_subtype"]
             symb = news_data["asset"]
             if subtype == "earnings":
+                
                 earnings = news_data["value"]
-                ### Do something with this data ###
+
+                # Do something with this data
+
             else:
+
+             
                 new_signatures = news_data["new_signatures"]
                 cumulative = news_data["cumulative"]
-                ### Do something with this data ###
+
+                # Do something with this data
         else:
-            ### Not sure what you would do with unstructured data.... ###
+
+            # Not sure what you would do with unstructured data....
+
             pass
 
+    async def trade(self):
+        await asyncio.sleep(5)
+        print("attempting to trade")
+        await self.place_order("APT",3, xchange_client.Side.BUY, 5)
+        await self.place_order("APT",3, xchange_client.Side.SELL, 7)
+        await asyncio.sleep(5)
+        await self.cancel_order(list(self.open_orders.keys())[0])
+        await self.place_swap_order('toAKAV', 1)
+        await asyncio.sleep(5)
+        await self.place_swap_order('fromAKAV', 1)
+        await asyncio.sleep(5)
+        await self.place_order("APT",1000, xchange_client.Side.SELL, 7)
+        await asyncio.sleep(5)
+        market_order_id = await self.place_order("APT",10, xchange_client.Side.SELL)
+        print("MARKET ORDER ID:", market_order_id)
+        await asyncio.sleep(5)
+        print("my positions:", self.positions)
 
-print('bruh')
+    async def view_books(self):
+        while True:
+            await asyncio.sleep(3)
+            for security, book in self.order_books.items():
+                sorted_bids = sorted((k,v) for k,v in book.bids.items() if v != 0)
+                sorted_asks = sorted((k,v) for k,v in book.asks.items() if v != 0)
+                print(f"Bids for {security}:\n{sorted_bids}")
+                print(f"Asks for {security}:\n{sorted_asks}")
+
+    async def start(self, user_interface):
+        asyncio.create_task(self.trade())
+
+        # This is where Phoenixhood will be launched if desired. There is no need to change these
+        # lines, you can either remove the if or delete the whole thing depending on your purposes.
+        if user_interface:
+            self.launch_user_interface()
+            asyncio.create_task(self.handle_queued_messages())
+
+        await self.connect()
+
+
+async def main(user_interface: bool):
+    # SERVER = '127.0.0.1:8000'   # run locally
+    SERVER = 'SERVER URL'
+    my_client = MyXchangeClient(SERVER,"USERNAME","PASSWORD")
+    await my_client.start(user_interface)
+    return
+
+if __name__ == "__main__":
+
+    # This parsing is unnecessary if you know whether you are using Phoenixhood.
+    # It is included here so you can see how one might start the API.
+
+    parser = argparse.ArgumentParser(
+        description="Script that connects client to exchange, runs algorithmic trading logic, and optionally deploys Phoenixhood"
+    )
+
+    parser.add_argument("--phoenixhood", required=False, default=False, type=bool, help="Starts phoenixhood API if true")
+    args = parser.parse_args()
+
+    user_interface = args.phoenixhood
+
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(main(user_interface))
 
 
