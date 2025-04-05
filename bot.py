7bba7f355ad8c5017415c5495817fa7aa0140f8a
Sriram Ananthakrishnan <srirama1@uchicago.edu>
2025-04-05 17:51:10 -0500

    dlr/apt pricing, bot not functional

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from utcxchangelib import xchange_client
+import numpy as np
 import asyncio
 import argparse
 
@@ -44,63 +45,116 @@ class MyXchangeClient(xchange_client.XChangeClient):
             subtype = news_data["structured_subtype"]
             symb = news_data["asset"]
             if subtype == "earnings":
-                
                 earnings = news_data["value"]
-
-                # Do something with this data
-
+                self.fair_prices['APT'] = 100 * earnings #may have to change this value during the actual competition
+                #trade around this value
             else:
-
-             
+                
                 new_signatures = news_data["new_signatures"]
                 cumulative = news_data["cumulative"]
+                #S_i = lognormal(log a + log S_(i-1), sigma^2)
+                #S_0 = 5000, alpha = 1.0630449594499
+                #sigma = 0.006
+                alpha = 1.0630449594499
+                sigma = 0.006
+                log_alpha = np.log(alpha)
+                remaining_sigs = 100000 - cumulative
+                ev_per_next_rds = new_signatures * alpha
+                rounds_remaining = 50 - self.DLR_updates
 
+                good_sims = 0
+                #just gonna do a monte carlo here lol
+                for i in range(1000): #1000 sims
+                    init_new_sigs = new_signatures
+                    for j in range(rounds_remaining):
+                        mu = np.log(init_new_sigs) + log_alpha
+                        init_new_sigs = np.random.lognormal(mean=mu, sigma=0.006)
+                    good_sims += (init_new_sigs >= 100000)
+
+                self.fair_prices['DLR'] = good_sims * 10
                 # Do something with this data
+                # EV = 100 * p(will reach 100,000 sigs), so trade around this fair price
+
         else:
 
             # Not sure what you would do with unstructured data....
-
+            # Think this has to do with click trading, actually. 
             pass
 
     #super simple strategy implementation
     #for every trade that's made, update fair price to most recently transacted value
     #then offer bid/ask around that value
     async def trade(self):
-        await asyncio.sleep(5)
-        for i in range(200): 
-            akav_sum_buy_price, akav_sum_sell_price = 0, 0
-            print(f"{self.spreads}")
-            for asset in ['APT', 'DLR', 'MKJ']:
-                if asset in self.spreads and (self.spreads)[asset][1][0] - (self.spreads)[asset][0][0] > 2: 
-                    #for latency purposes, might want more efficient ways of getting min/max
-                    #might also want to incorporate fair price evals into this
-                    market_buy_id = await self.place_order(asset, 3, xchange_client.Side.BUY, (self.spreads)[asset][0][0]+1)
-                    akav_sum_buy_price += (self.spreads)[asset][0][0]
-                    market_sell_id = await self.place_order(asset, 3, xchange_client.Side.SELL, (self.spreads)[asset][1][0]-1)
-                    akav_sum_sell_price += (self.spreads)[asset][1][0]
-                    print(f"ORDERS PLACED FOR {asset}, buy at {(self.spreads)[asset][0][0]+1}, sell at {(self.spreads)[asset][1][0]-1}")
+        await asyncio.sleep(5)  
+        self.positions['AKAV'] = 0
+        self.positions['AKIM'] = 0
+        self.fair_prices['DLR'] = 5000
+        while True:
+
+            await asyncio.sleep(1)
+            self.spreads = {}
+            for symbol in ['APT', 'DLR', 'MKJ', 'AKAV', 'AKIM']:
+                bids = self.order_books[symbol].bids
+                asks = self.order_books[symbol].asks
+                bids_sorted = sorted((k,v) for k,v in bids.items() if v != 0)
+                asks_sorted = sorted((k,v) for k,v in asks.items() if v != 0)
+                if bids_sorted and asks_sorted:
+                    self.spreads[symbol] = (bids_sorted[0], asks_sorted[-1])
+                print(f"Bids for {symbol} are {bids_sorted}\n")
+                print(f"Asks for {symbol} are {asks_sorted}\n")
+            print(f"Spreads are {self.spreads}\n")
+            for asset in ['APT', 'DLR']:
+                fair_price = self.fair_price[asset]
+                market_buy_id = await self.place_order(asset, 4, xchange_client.Side.BUY, fair_price-2)
+                market_sell_id = await self.place_order(asset, 4, xchange_client.Side.SELL, fair_price + 2)
+                # if asset in self.spreads and (self.spreads)[asset][1][0] - (self.spreads)[asset][0][0] > 2: 
+                #     #for latency purposes, might want more efficient ways of getting min/max
+                #     #might also want to incorporate fair price evals into this 
+                #     #OR we could just let other people do it for us (which is what this strat takes advantage of)
+
+                #     market_buy_id = await self.place_order(asset, 4, xchange_client.Side.BUY, (self.spreads)[asset][0][0]+1)
+                #     akav_sum_buy_price += (self.spreads)[asset][0][0] + 1
+                #     market_sell_id = await self.place_order(asset, 4, xchange_client.Side.SELL, (self.spreads)[asset][1][0]-1)
+                #     akav_sum_sell_price += (self.spreads)[asset][1][0] - 1
+                #     print(f"ORDERS PLACED FOR {asset}, buy at {(self.spreads)[asset][0][0]+1}, sell at {(self.spreads)[asset][1][0]-1}")
+                    
+                    # Strat: frontrun existing bid/asks
+                    # Look at the best bid, add 1, look at the best sell, subtract 1
+                    # Obviously, don't do this if best sell - best bid < 2, which I'm assuming it will be during the competition
+                    # However, as orders fill, our outstanding orders will gradually get processed, which is arb over time
+                    # Simplicity = good type shi
+
 
             # etf_mm = True
             # for asset in ['APT', 'DLR', 'MKJ', 'AKAV']:
             #     if asset not in self.spreads:
             #         etf_mm = False
+            # #Note: do not put any sleep() statements here bc orderbooks might update too fast
+            # #We want most updated prices
 
+            # #our fair buy price = best bid + 1
+            # #our fair sell price = best ask - 1
+            # #if we have an etf at best bid + 1, it's optimal to buy if best bid + 1 (etf) + 5 < sum (fair buy prices)
+            # #if we have an etf at best_ask - 1, it's optimal to sell if sum (fair sell prices) + 5 < best_sell -1
             # if etf_mm:
-            #     #do stuff w/ etfs.
-            #     if akav_sum_buy_price + 5 < (self.spreads)['AKAV'][0][0]:
+            #     #do stuff w/ etfs. for testing purposes
+            #     print("\nETF market making\n")
+            #     if (self.spreads)['AKAV'][0][0] + 1 + 5 < akav_sum_buy_price:
             #         #buy etf, convert it back to individual stocks, buy them on exchange
+            #         #math time: if we buy etf for price p -> convert to x, y, z (our fair prices for buying)
+            #         self.place_order('AKAV', 1, xchange_client.Side.BUY, (self.spreads)['AKAV'][0][0] + 1)
+            #         if self.positions['AKAV'] > 0:
+            #             #assuming its bought, convert 1 share to stocks (idk if this is needed)
+            #             self.place_swap_order('fromAKAV', 1)
+            #     elif akav_sum_sell_price + 5 < (self.spreads)['AKAV'][1][0] - 1:
             #         self.place_swap_order('toAKAV', 1)
-                
+            #         self.place_order('AKAV', 1, xchange_client.Side.SELL, (self.spreads)['AKAV'][1][0] -1)
 
-
-
-            #This somehow gives positive PNL against the bots lmao^^^
+            #This gives fairly good PNL against bots, but this is because bots are dumb
             #Next: figuring out how to price assets in a better manner
             #Also, figure out how to price ETFs. Might want to learn how to hedge AKIM w/ AKAV
-
-
-            await asyncio.sleep(3)
             print("my positions:", self.positions)
+            await asyncio.sleep(2)
         # await self.place_order("APT",3, xchange_client.Side.BUY, 5)
         # await self.place_order("APT",3, xchange_client.Side.SELL, 7)
         # await asyncio.sleep(5)
